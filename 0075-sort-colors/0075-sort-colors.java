6b962e6996137f314a1f617b8f2b51e3b6a4538e
VIVEK SHARMA <sharmavivek93897@gmail.com>
2026-08-08 15:05:20 +0530

    Time: 0 ms (100%), Space: 43.7 MB (25.22%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int left = 0;
+        int mid = 0;
+        int right = nums.length-1;
+        while(mid<=right){
+
+            if(nums[mid]==0){
+                //three pointer
+                int temp = nums[left];
+                nums[left] = nums[mid];
+                nums[mid] = temp;
+                left++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                int temp = nums[mid];
+                nums[mid] = nums[right];
+                nums[right] = temp;
+                right--;
+            }
+        }
+    }
+}
