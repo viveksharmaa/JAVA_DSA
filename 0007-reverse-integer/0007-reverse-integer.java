82dc820d60107a9e2431d1305dcf518bc80b3722
VIVEK SHARMA <sharmavivek93897@gmail.com>
2026-08-13 10:37:23 +0530

    Time: 1 ms (99.98%), Space: 42.6 MB (64.14%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -7,13 +7,13 @@ class Solution {
             x = x / 10;
 
             // Check overflow BEFORE multiplying
-            if (rev > Integer.MAX_VALUE / 10 ) {
+            if (rev > Integer.MAX_VALUE / 10 || rev < Integer.MIN_VALUE / 10 ) {
                 return 0;
             }
 
-            if (rev < Integer.MIN_VALUE / 10  ) {
-                return 0;
-            }
+            // if (rev < Integer.MIN_VALUE / 10  ) {
+            //     return 0;
+            // }
 
             rev = rev * 10 + digit;
         }
