f1b544438c94fbeefa452c82e14dd70002d4dbb9
VIVEK SHARMA <sharmavivek93897@gmail.com>
2026-08-22 22:06:24 +0530

    Time: 7 ms (17.5%), Space: 45.2 MB (41.17%) - LeetHub

---
+++ 0948-bag-of-tokens/0948-bag-of-tokens.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int bagOfTokensScore(int[] tokens, int power) {
+        Arrays.sort(tokens);
+        int left = 0;
+        int right = tokens.length-1;
+
+        int score = 0;
+        int maxScore = 0;
+        while(left <= right){
+            if(power >= tokens[left]){
+                power -= tokens[left];
+                left++;
+                score++;
+                maxScore = Math.max(maxScore, score);
+            }
+            else if(score > 0){
+                power += tokens[right];
+                right--;
+                score--;
+            }
+            else {
+                break;
+            }
+        }
+        return maxScore;
+    }
+}
