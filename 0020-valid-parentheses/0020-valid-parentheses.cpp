784a97a5dc94e22344c619592a922cae69ea5bbf
VIVEK SHARMA <sharmavivek93897@gmail.com>
2026-08-24 11:39:58 +0530

    Time: 3 ms (9.18%), Space: 10.2 MB (6.35%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isValid(string s) {
-                stack <char>st;
+                stack <int>st;
 
         for(int i=0;i<s.size();i++){
             if(s[i]=='(' ||s[i]=='{' ||s[i]=='[' ){
