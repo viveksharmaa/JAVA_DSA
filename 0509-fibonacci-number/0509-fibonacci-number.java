c11bf53669bcdf1e3a435a1636fd71860cb75c22
VIVEK SHARMA <sharmavivek93897@gmail.com>
2026-09-08 11:22:44 +0530

    Time: 0 ms (100%), Space: 42.2 MB (33.34%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int fibo(int n, int[] dp) {
+        if(n<=1) return n;
+        if(dp[n]!=0) return dp[n];
+        int ans = fibo(n-1 , dp) + fibo(n-2, dp);
+        dp[n] = ans;
+        return ans;
+    }
+    public int fib(int n){
+        int[] dp = new int[n+1];
+        return fibo(n, dp);
+    }
+}
