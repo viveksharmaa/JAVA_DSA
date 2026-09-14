bf203cb8b57411c9cf7077823807a3dd6f00c1d6
VIVEK SHARMA <sharmavivek93897@gmail.com>
2026-09-14 10:11:39 +0530

    Time: 14 ms (91.89%), Space: 46.4 MB (51.91%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount +1];
+
+        for(int i=0; i<=amount; i++){
+            dp[i] = amount+1;
+        }
+        dp[0] = 0;
+        for(int i=1; i<=amount; i++){
+            for(int coin : coins){
+                if(i>=coin){
+                    dp[i] = Math.min(dp[i], dp[i-coin] + 1);
+                }
+            }
+        }
+        if(dp[amount] == amount+1){
+            return -1;
+        }
+        return dp[amount];
+    }
+}
